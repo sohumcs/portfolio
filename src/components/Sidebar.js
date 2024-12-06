951916b7e6266b0325abc90e2b83a4460f9be8e0
Sohum Chandra Srivastava <sohum611@gmail.com>
2024-12-06 16:50:32 +0530

    Fixed resume download functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -29,14 +29,14 @@ const Sidebar = ({ openAbout, openProjects, openConnect }) => {
         <span className="icon-text">LinkedIn</span>
       </div>
       <div className="icon-container" onClick={() => {
-        const link = document.createElement('a');
-        link.href = "/resume-sohum.pdf";  // Correct relative path to the file in public folder
-        link.download = "Resume_Sohum.pdf";  // The file name for the downloaded file
-        link.click();
-      }}>
-        <FontAwesomeIcon icon={faBook} className="icon" />
-        <span className="icon-text">Resume</span>
-      </div>
+  const link = document.createElement('a');
+  link.href = '/resume-sohum.pdf';  // Ensure this is the correct path
+  link.download = 'resume-sohum.pdf';  // This will trigger the download with the specified filename
+  link.click();
+}}>
+  <FontAwesomeIcon icon={faBook} className="icon" />
+  <span className="icon-text">Resume</span>
+</div>
     </div>
   );
 };
